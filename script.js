55040aded50ebf834adb02149c9778cbf049e715
LEMON8188 <muhammadnabijonov4@gmail.com>
2025-08-20 18:22:10 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+let str = prompt('string malumotni kiriting')
+let num = prompt('raqam malumot turini kiriting')
+let bool = prompt('boolsen malumot turini kiriting')
+let Confirm = prompt('aniq togri yozdinmi?????')
+
+console.log(typeof str)
+console.log(typeof Number(num))
+console.log(typeof Boolean(bool))
+console.log(typeof Confirm)
+
+alert("string malumotni turi " + str + " type " + typeof str +
+    "\nnumber malumot turi " + num + " type " + typeof + Number(num) +
+    "\nboolean malumot turi " + bool + " type " + typeof Boolean(bool) +
+    "\naniq togri yozilganmi " + Confirm + " type " + typeof Confirm
+)
